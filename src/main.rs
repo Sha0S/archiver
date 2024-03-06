9132cf4c94c98c6883197a79764d367f920c4daf
Bernát Süli <suli.bernat@gmail.com>
2024-03-06 18:05:17 +0100

    Added mode, where it compresses per subdir

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use chrono::Local;
-use std::fs;
 use std::env;
+use std::fs;
 use std::path::Path;
 use std::process;
 use std::time::Instant;
 
-// Return is a vector of (source_dir, destination_dir)
-fn read_config() -> Vec<(String, String)> {
-    let mut ret: Vec<(String, String)> = Vec::new();
+/*
+Mode::OneArchive - compress the whole source directory to one archive (default)
+Mode::SubFolders - compress each sub-folder into separate archive
+*/
+enum Mode {
+    OneArchive,
+    SubFolders,
+}
+
+// Return is a vector of (source_dir, destination_dir(, mode))
+fn read_config() -> Vec<(String, String, Mode)> {
+    let mut ret: Vec<(String, String, Mode)> = Vec::new();
 
     let p = Path::new(".\\config");
     if let Ok(fileb) = fs::read_to_string(p) {
@@ -24,6 +33,17 @@ fn read_config() -> Vec<(String, String)> {
                         .next()
                         .expect("config file is formated incorrectly")
                         .to_owned(),
+                    match parts.next() {
+                        Some(x) => {
+                            if x == "S" {
+                                Mode::SubFolders
+                            } else {
+                                // Could check for wrong arg here.
+                                Mode::OneArchive
+                            }
+                        }
+                        None => Mode::OneArchive,
+                    },
                 ));
             }
         }
@@ -35,6 +55,8 @@ fn read_config() -> Vec<(String, String)> {
 fn main() -> Result<(), std::io::Error> {
     let config = read_config();
 
+    // default extension is .tar.zst
+    // passing a arg to the program will overwrite this
     let args: Vec<String> = env::args().collect();
     let extension: String = {
         if let Some(s) = args.get(1) {
@@ -44,8 +66,7 @@ fn main() -> Result<(), std::io::Error> {
         }
     };
 
-
-    for (source, destination) in config {
+    for (source, destination, mode) in config {
         // Sanitiy check:
         if !Path::new(&source).exists() {
             panic!("Source folder not found! {}", source);
@@ -56,34 +77,85 @@ fn main() -> Result<(), std::io::Error> {
             fs::create_dir_all(&destination)?;
         }
 
-        // Create output filename:
         let source_path = Path::new(&source);
-        let filename = format!(
-            "{}_{}{}",
-            source_path.file_name().unwrap().to_str().unwrap(), //this should not fail. It could only fail if it can't extract a filename from the Path.
-            Local::now().format("%y_%m_%d_%H_%M"),
-            extension
-        );
-        let destination_path = Path::new(&destination).join(Path::new(&filename));
 
-        println!("Starting {} -> {}", source_path.display(), destination_path.display());
-        let t = Instant::now();
+        match mode {
+            Mode::OneArchive => {
+                // Create output filename:
+                let filename = format!(
+                    "{}_{}{}",
+                    source_path.file_name().unwrap().to_str().unwrap(), //this should not fail. It could only fail if it can't extract a filename from the Path.
+                    Local::now().format("%y_%m_%d_%H_%M"),
+                    extension
+                );
+                let destination_path = Path::new(&destination).join(Path::new(&filename));
 
-        // Create the archive:
-        let status = process::Command::new("tar")
-            .args([
-                "-ca",
-                "-f",
-                destination_path.to_str().unwrap(),
-                "-C",
-                &source,
-                "."
-            ])
-            .status()
-            .expect("failed to execute process");
+                println!(
+                    "Starting {} -> {}",
+                    source_path.display(),
+                    destination_path.display()
+                );
+                let t = Instant::now();
 
-        println!("Status: {}", status);
-        println!("Time elapsed: {} sec", t.elapsed().as_secs());
+                // Create the archive:
+                let status = process::Command::new("tar")
+                    .args([
+                        "-ca",
+                        "-f",
+                        destination_path.to_str().unwrap(),
+                        "-C",
+                        &source,
+                        ".",
+                    ])
+                    .status()
+                    .expect("failed to execute process");
+
+                println!("Status: {}", status);
+                println!("Time elapsed: {} sec", t.elapsed().as_secs());
+            }
+
+            Mode::SubFolders => {
+                for entry in fs::read_dir(source)? {
+                    let entry = entry?;
+                    let path = entry.path();
+                    if path.is_dir() {
+                        // Create output filename:
+                        let filename = format!(
+                            "{}_{}{}",
+                            path.file_name().unwrap().to_str().unwrap(), //this should not fail. It could only fail if it can't extract a filename from the Path.
+                            Local::now().format("%y_%m_%d_%H_%M"),
+                            extension
+                        );
+                        let destination_path = Path::new(&destination).join(Path::new(&filename));
+
+                        println!(
+                            "Starting {} -> {}",
+                            path.display(),
+                            destination_path.display()
+                        );
+                        let t = Instant::now();
+
+                        // Create the archive:
+                        let status = process::Command::new("tar")
+                            .args([
+                                "-ca",
+                                "-f",
+                                destination_path.to_str().unwrap(),
+                                "-C",
+                                path.to_str().unwrap(),
+                                ".",
+                            ])
+                            .status()
+                            .expect("failed to execute process");
+
+                        println!("Status: {}", status);
+                        println!("Time elapsed: {} sec", t.elapsed().as_secs());
+                    }
+
+                    // Should we care about singular files?
+                }
+            }
+        }
     }
 
     Ok(())
